db300fe47802fc8edd97a03fddf4ec1df58e0627
udayammi11 <88530120+udayammi11@users.noreply.github.com>
2025-11-13 14:48:34 +0100

    Update text in ClsComp.js description

--- src/components/ClsComp.js
+++ src/components/ClsComp.js
@@ -7,7 +7,7 @@ class ClsComp extends Component {
 	2.	Text (p):white-space: nowrap; prevents text from wrapping to a new line. animation: scroll-right 10s linear infinite; animates the text from right to left over 10 seconds in an infinite loop.
 	3.	Keyframes (@keyframes scroll-right):	transform: translateX(100%); starts the text just outside the right edge of the viewport. transform: translateX(-100%); moves it completely past the left edge.
 	4.	Animation Speed:The 10s in animation defines how long it takes for one full scroll. Adjust this value to make the text scroll faster or slower.
-	5.  Just added.asdfghj
+	5.  Just added.asdfghjvbdgfb
     </p>
         </div>)
     }
